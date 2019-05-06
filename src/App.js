232ed8e8d7a002513ea7abafbc86e0058ea77a3c
Milan Koniar <milankoniar@gmail.com>
2019-05-06 13:58:59 +0200

    added (partially dynamic) styling to todoItem

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         {
           id: 2,
           title: 'title 2',
-          completed: false
+          completed: true
         },
         {
           id: 3,
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+
+    getStyle = () => {
+        return {
+            background: '#eee',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }
+    }
+
   render() {
     return (
-      <div>
+      <div style={this.getStyle()}>
         {this.props.todo.title}
       </div>
     )
